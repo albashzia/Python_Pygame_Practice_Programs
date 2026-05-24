287f1bb4f08d040cc2bd7010baf8c5880ec56a4b
albashzia <ahmedalbashzia@gmail.com>
2026-05-24 12:16:09 +0500

    Defining a loop to make the surface visible on the screen and exit on Quit

--- Code/game_loop.py
+++ Code/game_loop.py
@@ -5,3 +5,11 @@ pygame.init()
 screen = pygame.display.set_mode((300,300))
 screen.fill("white")
 pygame.display.set_caption("First Pygame Program")
+
+done = True
+while done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = False
+
+    pygame.display.flip()
