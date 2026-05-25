be35bc895cec43e730a50dbb3c1f0e82ca6b5ec0
albashzia <ahmedalbashzia@gmail.com>
2026-05-25 22:30:29 +0500

    Implementing the logic to recognize the key pressed, change the value of variables and draw the rectangle on the screen as per those values

--- Code/key_events.py
+++ Code/key_events.py
@@ -6,19 +6,31 @@ screen_width = 500
 screen_height = 500
 
 screen = pygame.display.set_mode((screen_width,screen_height))
-screen.fill("white")
 pygame.display.set_caption("Key Events")
 
 x = 0
 y = 0
 rect_width = 50
 rect_height = 50
-velocity = 5
+velocity = 1
 
 done = True
 while done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        y -= velocity
+    if keys[pygame.K_DOWN]:
+        y += velocity
+    if keys[pygame.K_LEFT]:
+        x -= velocity
+    if keys[pygame.K_RIGHT]:
+        x += velocity
+
+    screen.fill("white")
+    pygame.draw.rect(screen,"black",(x,y,rect_width,rect_height))
 
     pygame.display.flip()
