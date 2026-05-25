aa6408cf1d17636d577ecaa49d887080ffce0628
albashzia <ahmedalbashzia@gmail.com>
2026-05-25 22:18:52 +0500

    Initializing a basic pygame screen in key_events.py code file

---
+++ Code/key_events.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+
+screen_width = 500
+screen_height = 500
+
+screen = pygame.display.set_mode((screen_width,screen_height))
+screen.fill("white")
+pygame.display.set_caption("Key Events")
+
+done = True
+while done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = False
+
+    pygame.display.flip()
