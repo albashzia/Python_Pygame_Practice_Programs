593d9ab75f4d52d457466fea3d7b2455a28d7644
albashzia <ahmedalbashzia@gmail.com>
2026-05-26 20:52:34 +0500

    Creating a rectangle and defining key events to move it on the screen

--- Code/boundaries.py
+++ Code/boundaries.py
@@ -7,10 +7,31 @@ screen_height = 500
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Defining Boundaries")
 
+x = 0
+y = 0
+rect_width = 50
+rect_height = 50
+velocity = 3
+
+clock = pygame.time.Clock()
+
 done = True
 while done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = False
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        y -= velocity
+    if keys[pygame.K_DOWN]:
+        y += velocity
+    if keys[pygame.K_LEFT]:
+        x -= velocity
+    if keys[pygame.K_RIGHT]:
+        x += velocity
+
+    screen.fill("white")
+    pygame.draw.rect(screen,"black",(x,y,rect_width,rect_height))
+    clock.tick(60)
     pygame.display.flip()
